8f493eb774770d8dbfcb7aafb141c680274a91d4
agata <agata.piasecka@outlook.com>
2025-05-16 01:07:46 +0200

    removed old kafka consumer

--- app/kafka.py
+++ app/kafka.py
@@ -21,44 +21,6 @@ class KafkaProducerSingleton:
         producer.produce(topic, message)
         producer.flush()
 
-class KafkaConsumerSingleton:
-    _consumer = None
-
-    @classmethod
-    def get_consumer(cls):
-        if cls._consumer is None:
-            cls._consumer = Consumer({
-                'bootstrap.servers': kafka_bootstrap_servers,
-                'group.id': 'kitchen-consumer-group',
-                'auto.offset.reset': 'earliest'  # or 'latest'
-            })
-        return cls._consumer
-
-    @classmethod
-    def consume_message(cls, topics):
-        consumer = cls.get_consumer()
-        consumer.subscribe(topics)
-        try:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None:
-                return None
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
-                    print(f"End of partition reached {msg.partition}, offset {msg.offset}")
-                else:
-                    print(f"Error: {msg.error()}")
-            else:
-                return msg.value().decode('utf-8')
-        except Exception as e:
-            print(f"Error consuming message: {e}")
-            return None
-
-    @classmethod
-    def close(cls):
-        if cls._consumer:
-            cls._consumer.close()
-
-
 def setup_topic(topic_name: str):
     admin_client = KafkaAdminClient(
         bootstrap_servers=kafka_bootstrap_servers,
